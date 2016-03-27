149ca1bf6de30f4a89f8550973dfe774116b17aa
Phat <vanphat@siliconstraits.com>
2016-03-27 15:45:49 +0700

    related hoses

--- app/modules/hose/hoseDetailsController.js
+++ app/modules/hose/hoseDetailsController.js
@@ -9,11 +9,18 @@
     .controller('hoseDetailsController', ['$stateParams', 'hoseService',
       function($stateParams, hoseService) {
         var vm = this;
+        vm.hose = {};
+        vm.relatedHoses = [];
 
         function init() {
           hoseService.getHoseById($stateParams.hoseId)
             .then(function(response) {
               vm.hose = response;
+            });
+
+          hoseService.getHoses()
+            .then(function(response) {
+              vm.relatedHoses = response;
             })
         }
 
