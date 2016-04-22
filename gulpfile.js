c8387251485e10f08646de2437dd5fa16121c528
Phat <vanphat@siliconstraits.com>
2016-04-23 04:51:46 +0700

    config browser-sync but fail (404 not found)

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,64 @@
 var gulp = require('gulp'),
-  $ = require('gulp-load-plugins')();
+  $ = require('gulp-load-plugins')(),
+  bowerFiles = require('main-bower-files'),
+  browserSync = require('browser-sync'),
+  historyApiFallback = require('connect-history-api-fallback');
 
-gulp.task('css', function() {
+var paths = {
+  app: './app',
+  assets: './app/assets',
+  css: './app/assets/css',
+  less: './app/assets/less',
+  js: './app/assets/js'
+};
+
+var devScripts = [
+  paths.app + '/*.js',
+  paths.app + '/modules/**/*.js'
+];
+
+gulp.task('css', function () {
   return gulp
-    .src('./app/assets/less/app.less')
+    .src(paths.less + '/app.less')
     .pipe($.less())
-    .pipe(gulp.dest('./app/assets/css'))
+    .pipe(gulp.dest(paths.css))
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
   return gulp
-    .src('./index.html')
-    .pipe($.inject(gulp.src('./app/**/*.js').pipe($.angularFilesort())))
-    .pipe(gulp.dest('./'));
+    .src(bowerFiles())
+    .pipe($.concat('vendor.js'))
+    .pipe(gulp.dest(paths.js));
 });
 
-gulp.task('watch', function() {
+gulp.task('inject', ['js'], function () {
   return gulp
-    .watch('./app/assets/less/*.less', ['css']);
+    .src(paths.app + '/index.html')
+    .pipe($.inject(gulp.src(devScripts).pipe($.angularFilesort())))
+    .pipe(gulp.dest(paths.app));
 });
 
-gulp.task('default', ['css', 'js']);
+gulp.task('watch', function () {
+  return gulp
+    .watch(paths.less + '/*.less', ['css']);
+});
+
+// gulp.task('sync', function () {
+//
+//   // Configurations
+//   browserSync.init({
+//     server: {
+//       baseDir: paths.app,
+//       middleware: [historyApiFallback()]
+//     }
+//   });
+//
+//   // Watch html/css/js changes and reload the app
+//   gulp.watch([
+//     paths.app + '/**/*.html',
+//     paths.css + '/app.css',
+//     devScripts
+//   ]).on('change', browserSync.reload)
+// });
+
+gulp.task('default', ['css', 'js', 'inject']);
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express'),
 app.use(express.static(__dirname)); // Serve files
 
 app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/app/index.html');
 });
 
 app.listen(port);
